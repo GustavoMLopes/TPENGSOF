b609a460451d1fc6e5bff42c1f02755df79755cb
Gustavo Martins <gumartinslopes@gmail.com>
2022-10-13 20:02:01 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
-const password = 1234 
-mongoose.connect(`mongodb+srv://gustavo:${password}@clusteragendaes.hep0uha.mongodb.net/agenda`)
+const password = 123
+//mongoose.connect(`mongodb+srv://gustavo:${password}@clusteragendaes.hep0uha.mongodb.net/agenda`)
+mongoose.connect(`mongodb+srv://daniel:${password}@cluster0.a78vqo1.mongodb.net/agenda`)
+    
 let db = mongoose.connection
 
 export default db
